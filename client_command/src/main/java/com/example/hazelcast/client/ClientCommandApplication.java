f16658a34a36e5e487ed591e9532f67abb31d61e
Miguel <mjbvilhena@outlook.com>
2018-02-02 12:03:12 +0000

    adding Swagger support
    APIs can now also be called from:
    
    Client_Command:
    http://localhost:8088/swagger-ui.html
    
    Client_Query:
    http://localhost:8089/swagger-ui.html

--- client_command/src/main/java/com/example/hazelcast/client/ClientCommandApplication.java
+++ client_command/src/main/java/com/example/hazelcast/client/ClientCommandApplication.java
@@ -9,6 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EntityScan("com.example.hazelcast.shared.model")
@@ -33,5 +38,17 @@ public class ClientCommandApplication {
 		return HazelcastClient.newHazelcastClient(clientConfig);
 	}
 
+    @Configuration
+    @EnableSwagger2
+    public class SwaggerConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
+    }
 
 }
